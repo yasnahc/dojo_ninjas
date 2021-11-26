0d1d62f228d6e695c8c4ddf0295ad7d45db3a0ad
Yasna Hormazabal <yasnahormazabalc@gmail.com>
2021-11-26 01:57:10 -0300

    proyecto terminado

--- app/models/dojo.rb
+++ app/models/dojo.rb
@@ -1,2 +1,7 @@
 class Dojo < ApplicationRecord
+    has_many :ninjas, dependent: :destroy
+    validates :name, :city, :state, presence: true
+    validates :state, length: {minimum: 2, maximum: 2} 
+
+   
 end
